ef64b46fa23ee4f69c72384f00e5e50097005650
EsratHossan <esrathossan7@gmail.com>
2023-02-06 19:55:08 +0600

    first commit

---
+++ parameter.js
@@ -0,0 +1,21 @@
+function bringSIngara(money){
+    console.log('tk disen:', money);
+    console.log('ei nen singara');
+    
+
+}
+
+// bringSIngara(250);
+var taka = 300;
+
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a+b+c+d+e;
+    console.log(sum);
+}
+sum(15, 96,  56, 17, 20);
